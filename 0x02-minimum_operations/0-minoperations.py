a870174b4d63d66daab0a1b7f7e89f56a290d569
Lexzino <alexudeze001@gmail.com>
2024-07-11 16:10:33 +0100

    task completed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,21 @@
-0-minoperations.py
+#!/usr/bin/python3
+""" Module for 0-minoperations"""
+
+def minOperations(n):
+    """
+    Returns the fewest number of operations needed
+    to result in exactly n H characters
+    """
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
